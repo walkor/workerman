2a5beabe61c32ccff92f71b6dfd0cdac54e3e402
walkor <worker-man@qq.com>
2013-12-15 21:18:03 +0800

    ci

--- Workers/GameWorker.php
+++ Workers/GameWorker.php
@@ -36,7 +36,7 @@ class GameWorker extends WORKERMAN\Core\SocketWorker
                 switch($this->data['sub_cmd'])
                 {
                     case GameBuffer::SCMD_ON_CONNECT:
-                        call_user_func_array(array('Event', 'onConnect'), array($this->currentClientAddress, $this->data['from_uid'], $this->data['body']));
+                        call_user_func_array(array('Event', 'onConnect'), array($this->mainSocket, $this->data['from_uid'], $this->data['body']));
                         return; 
                 }
             }
