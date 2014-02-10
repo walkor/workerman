d0373d8f6b60373a9240613468ed62e176797c9a
walkor <worker-man@qq.com>
2014-02-10 23:27:41 +0800

    ci

--- applications/Statistics/Lib/Cache.php
+++ applications/Statistics/Lib/Cache.php
@@ -2,7 +2,7 @@
 class Cache
 {
     public static $statisticDataCache = array();
-    public static $ServerIpList = array();
+    public static $ServerIpList = array('127.0.0.1');
     public static $modulesDataCache = array();
     public static $lastFailedIpArray = array();
     public static $lastSuccessIpArray = array();
