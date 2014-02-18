70ed92feca77d258dc3684672bdcf0a28cfa096a
walkor <worker-man@qq.com>
2014-02-18 23:26:29 +0800

    ci

--- applications/Statistics/Web/index.php
+++ applications/Statistics/Web/index.php
@@ -21,7 +21,7 @@ $interface = isset($_GET['interface']) ? $_GET['interface'] : '';
 $date = isset($_GET['date']) ? $_GET['date'] : date('Y-m-d');
 $start_time = isset($_GET['start_time']) ? $_GET['start_time'] : strtotime(date('Y-m-d'));
 $offset =  isset($_GET['offset']) ? $_GET['offset'] : 0; 
-$log_count_per_page = 40;
+$log_count_per_ip = $log_count_per_page = 40;
 if(empty($_GET['count']) && $ip_count = count(\Statistics\Lib\Cache::$ServerIpList))
 {
     $log_count_per_ip = ceil($log_count_per_page/$ip_count); 
