731d82df3b70f40da419cb254ad727e5552ca28a
walkor <worker-man@qq.com>
2014-10-10 20:30:41 +0800

    mysql class fix

--- applications/Demo/Lib/Db.php
+++ applications/Demo/Lib/Db.php
@@ -28,7 +28,7 @@ class Db
         if(empty(self::$instance[$config_name]))
         {
             $config = \Config\Db::$$config_name;
-            self::$instance[$config_name] = new \Lib\DbConnection($config['host'], $config['port'], $config['user'], $config['password'], $config['db_name']);
+            self::$instance[$config_name] = new \Lib\DbConnection($config['host'], $config['port'], $config['user'], $config['password'], $config['dbname']);
         }
         return self::$instance[$config_name];
     }
