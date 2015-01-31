2d5209154eab028bd6d732d399e04d7352489d86
walkor <worker-man@qq.com>
2015-01-31 21:04:05 +0800

    Applications/*/start.php

--- start.php
+++ start.php
@@ -9,7 +9,7 @@ use WorkerMan\Worker;
 
 require_once __DIR__ . '/Workerman/Autoloader.php';
 
-foreach(glob(__DIR__.'/applications/*/start.php') as $start_file)
+foreach(glob(__DIR__.'/Applications/*/start.php') as $start_file)
 {
     require_once $start_file;;
 }
