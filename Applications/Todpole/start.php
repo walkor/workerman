b36b538668e370c3a2412e144811fa8cfeb409c2
walkor <worker-man@qq.com>
2015-03-01 20:13:43 +0800

    onWebsocketConnect example

--- Applications/Todpole/start.php
+++ Applications/Todpole/start.php
@@ -18,6 +18,24 @@ $gateway->pingInterval = 10;
 
 $gateway->pingData = '{"type":"ping"}';
 
+/* 
+// 当客户端连接上来时，设置连接的onWebSocketConnect，即在websocket握手时的回调
+$gateway->onConnect = function($connection)
+{
+    $connection->onWebSocketConnect = function($connection , $http_header)
+    {
+        // 可以在这里判断连接来源是否合法，不合法就关掉连接
+        // $_SERVER['SERVER_NAME']标识来自哪个站点的页面发起的websocket链接
+        if($_SERVER['SERVER_NAME'] != 'kedou.workerman.net')
+        {
+            $connection->close();
+        }
+        // onWebSocketConnect 里面$_GET $_SERVER是可用的
+        // var_dump($_GET, $_SERVER);
+    };
+}; 
+*/
+
 
 // bussinessWorker
 $worker = new BusinessWorker();
