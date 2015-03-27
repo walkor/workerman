8514c8769e8fa9c075186de8c92b828d87d9acee
walkor <worker-man@qq.com>
2015-03-27 22:05:43 +0800

    Autoloader::setRootPath(__DIR__);

--- Applications/Todpole/start.php
+++ Applications/Todpole/start.php
@@ -3,9 +3,11 @@ use \Workerman\Worker;
 use \Workerman\WebServer;
 use \GatewayWorker\Gateway;
 use \GatewayWorker\BusinessWorker;
+use \Workerman\Autoloader;
 
 // 自动加载类
 require_once __DIR__ . '/../../Workerman/Autoloader.php';
+Autoloader::setRootPath(__DIR__);
 
 // gateway 进程
 $gateway = new Gateway("Websocket://0.0.0.0:8585");
