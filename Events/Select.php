133201a2ff50fea4ebca3d35404edb55d7551c19
walkor <worker-man@qq.com>
2015-07-13 00:01:11 +0800

    timer first

--- Events/Select.php
+++ Events/Select.php
@@ -246,6 +246,12 @@ class Select implements EventInterface
             // 等待可读或者可写事件
             @stream_select($read, $write, $e, 0, $this->_selectTimeout);
             
+            // 尝试执行定时任务
+            if(!$this->_scheduler->isEmpty())
+            {
+                $this->tick();
+            }
+            
             // 这些描述符可读，执行对应描述符的读回调函数
             if($read)
             {
@@ -270,12 +276,6 @@ class Select implements EventInterface
                         call_user_func_array($this->_allEvents[$fd_key][self::EV_WRITE][0], array($this->_allEvents[$fd_key][self::EV_WRITE][1]));
                     }
                 }
-            }
-            
-            // 尝试执行定时任务
-            if(!$this->_scheduler->isEmpty())
-            {
-                $this->tick();
             }
         }
     }
