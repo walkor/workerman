e13707c5c0a345b6eaa7dcc5184ff118261b867c
walkor <walkor@users.noreply.github.com>
2016-02-04 22:46:11 +0800

    Update Ev.php

--- Events/Ev.php
+++ Events/Ev.php
@@ -6,8 +6,7 @@
  * For full copyright and license information, please see the MIT-LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @author walkor<walkor@workerman.net>
- * @copyright walkor<walkor@workerman.net>
+ * @author 有个鬼<42765633@qq.com>
  * @link http://www.workerman.net/
  * @license http://www.opensource.org/licenses/mit-license.php MIT License
  */
