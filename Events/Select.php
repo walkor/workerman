70583984c977fbcf9663e9da943cae359df17463
mrchenzifan <mrchenzifan@163.com>
2021-09-26 17:17:48 +0800

    Update Select.php
    update php8.1 Deprecated: Implicit conversion from float 9999.999 to int loses precision

--- Events/Select.php
+++ Events/Select.php
@@ -273,7 +273,7 @@ class Select implements EventInterface
                 } catch (\Exception $e) {} catch (\Error $e) {}
 
             } else {
-                usleep($this->_selectTimeout);
+                usleep((int)$this->_selectTimeout);
                 $ret = false;
             }
 
