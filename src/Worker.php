aa1b3d2793347e6453fd9e57eb6fd300dd6091d1
mowangjuanzi <baoguoxiao0538@hotmail.com>
2022-03-08 12:14:09 +0800

    optimizations

--- src/Worker.php
+++ src/Worker.php
@@ -1560,13 +1560,7 @@ class Worker
     protected static function setProcessTitle($title)
     {
         \set_error_handler(function(){});
-        // >=php 5.5
-        if (\function_exists('cli_set_process_title')) {
-            \cli_set_process_title($title);
-        } // Need proctitle when php<=5.5 .
-        elseif (\extension_loaded('proctitle') && \function_exists('setproctitle')) {
-            \setproctitle($title);
-        }
+        \cli_set_process_title($title);
         \restore_error_handler();
     }
 
