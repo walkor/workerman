f9fe8b6bfa14bba3fb0c2a7b94d61f70c602b868
Amir <80312616+amirfaramarzi@users.noreply.github.com>
2022-06-30 16:28:35 +0430

    fix Protocols/Http.php
    remove(Server: workerman) header

--- src/Protocols/Http.php
+++ src/Protocols/Http.php
@@ -193,7 +193,7 @@ class Http
                 unset($connection->__header);
             }
             $body_len = \strlen((string)$response);
-            return "HTTP/1.1 200 OK\r\nServer: workerman\r\n{$ext_header}Connection: keep-alive\r\nContent-Type: text/html;charset=utf-8\r\nContent-Length: $body_len\r\n\r\n$response";
+            return "HTTP/1.1 200 OK\r\n{$ext_header}Connection: keep-alive\r\nContent-Type: text/html;charset=utf-8\r\nContent-Length: $body_len\r\n\r\n$response";
         }
 
         if (isset($connection->__header)) {
