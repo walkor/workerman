a1891de40e4e1556371d73636484d625f9bd4468
Joanhey <joanhey@kumbiaphp.com>
2022-10-19 16:30:00 +0200

    Use constant for error type

--- src/Worker.php
+++ src/Worker.php
@@ -491,9 +491,9 @@ class Worker
     /**
      * PHP built-in error types.
      *
-     * @var array
+     * @var array<int,string>
      */
-    protected static $_errorType = [
+    const ERROR_TYPE = [
         \E_ERROR             => 'E_ERROR',             // 1
         \E_WARNING           => 'E_WARNING',           // 2
         \E_PARSE             => 'E_PARSE',             // 4
@@ -2076,8 +2076,8 @@ class Worker
      */
     protected static function getErrorType($type)
     {
-        if(isset(self::$_errorType[$type])) {
-            return self::$_errorType[$type];
+        if(isset(self::ERROR_TYPE[$type])) {
+            return self::ERROR_TYPE[$type];
         }
 
         return '';
