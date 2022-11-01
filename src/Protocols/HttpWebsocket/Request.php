706b8f846df1743b0b0325b440a435583091ea7c
tianyiw <tianyiw@vip.qq.com>
2022-11-02 00:36:45 +0800

    Listen to http and websocket at the same worker

---
+++ src/Protocols/HttpWebsocket/Request.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of workerman.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    walkor<walkor@workerman.net>
+ * @copyright walkor<walkor@workerman.net>
+ * @link      http://www.workerman.net/
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Workerman\Protocols\HttpWebsocket;
+
+/**
+ * Class Request
+ * @package Workerman\Protocols\HttpWebsocket
+ */
+class Request extends \Workerman\Protocols\Http\Request
+{
+
+    /**
+     * Received message content
+     *
+     * @var array
+     */
+    protected $_message;
+
+
+    /**
+     * Request constructor.
+     *
+     * @param string $buffer
+     * @param string $message
+     */
+    public function __construct($buffer, $message)
+    {
+        $this->_buffer = $buffer;
+        $this->_message = $message;
+    }
+
+    /**
+     * Get received message content.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return $this->_message;
+    }
+}
