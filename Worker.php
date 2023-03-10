fd77ca4c1e7ff083748e927e889965f3c608c027
walkor <walkor@users.noreply.github.com>
2023-03-10 21:50:53 +0800

    Fix #889

--- Worker.php
+++ Worker.php
@@ -943,7 +943,7 @@ class Worker
             exit;
         }
 
-        $statistics_file =  static::$statusFile ? static::$statusFile : __DIR__ . "/../workerman-$master_pid.status";
+        $statistics_file =  static::$statusFile ? static::$statusFile : __DIR__ . "/../workerman-$master_pid.$command";
 
         // execute command.
         switch ($command) {
