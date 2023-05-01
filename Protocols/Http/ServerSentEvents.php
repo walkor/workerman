e967b79f95b9251a72acb971be05623ec1a51e83
walkor <walkor@workerman.net>
2023-05-01 10:12:20 +0800

    fix sse

--- Protocols/Http/ServerSentEvents.php
+++ Protocols/Http/ServerSentEvents.php
@@ -50,15 +50,15 @@ class ServerSentEvents
         if (isset($data['event'])) {
             $buffer .= "event: {$data['event']}\n";
         }
-        if (isset($data['data'])) {
-            $buffer .= 'data: ' . \str_replace("\n", "\ndata: ", $data['data']) . "\n\n";
-        }
         if (isset($data['id'])) {
             $buffer .= "id: {$data['id']}\n";
         }
         if (isset($data['retry'])) {
             $buffer .= "retry: {$data['retry']}\n";
         }
-        return $buffer;
+        if (isset($data['data'])) {
+            $buffer .= 'data: ' . str_replace("\n", "\ndata: ", $data['data']) . "\n";
+        }
+        return $buffer . "\n";
     }
 }
