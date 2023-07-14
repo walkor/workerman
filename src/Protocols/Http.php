568f0368958da527566c99ae0360d6d73d687dbc
Joanhey <joanhey@kumbiaphp.com>
2023-07-14 02:26:41 +0200

    Revert to string

--- src/Protocols/Http.php
+++ src/Protocols/Http.php
@@ -217,6 +217,7 @@ class Http
                 }
                 $connection->headers = [];
             }
+            $response = (string)$response;
             $bodyLen = strlen($response);
             return "HTTP/1.1 200 OK\r\nServer: workerman\r\n{$extHeader}Connection: keep-alive\r\nContent-Type: text/html;charset=utf-8\r\nContent-Length: $bodyLen\r\n\r\n$response";
         }
