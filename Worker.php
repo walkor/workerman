5ccd9d4c916f22824a66ecc99054bd8247ad7dcc
walkor <walkor@users.noreply.github.com>
2023-07-27 15:38:16 +0800

    Update Worker.php

--- Worker.php
+++ Worker.php
@@ -34,7 +34,7 @@ class Worker
      *
      * @var string
      */
-    const VERSION = '4.1.11';
+    const VERSION = '4.1.12';
 
     /**
      * Status starting.
@@ -1060,7 +1060,10 @@ class Worker
         }
         $status_str = '';
         $current_total_request = array();
-        $worker_info = \unserialize($info[0]);
+        $workerInfo = [];
+        try {
+            $workerInfo = unserialize($info[0], ['allowed_classes' => false]);
+        } catch (Throwable $exception) {}
         \ksort($worker_info, SORT_NUMERIC);
         unset($info[0]);
         $data_waiting_sort = array();
