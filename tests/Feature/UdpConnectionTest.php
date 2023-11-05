a144894b74f5985a70bca0d096a47ad1280e33f4
Anton <anton.z@live.com>
2023-11-05 22:24:46 +0800

    Udp and websocket server code stubs moved out to Stub directory

--- tests/Feature/UdpConnectionTest.php
+++ tests/Feature/UdpConnectionTest.php
@@ -1,29 +1,12 @@
 <?php
 
 use Symfony\Component\Process\PhpProcess;
-use Workerman\Worker;
 
-$serverAddress = 'udp://127.0.0.1:6789';
 $process = null;
-beforeAll(function () use ($serverAddress, &$process) {
-    $process = new PhpProcess(<<<PHP
-        <?php    
-        if(!defined('STDIN')) define('STDIN', fopen('php://stdin', 'r'));
-        if(!defined('STDOUT')) define('STDOUT', fopen('php://stdout', 'w'));
-        if(!defined('STDERR')) define('STDERR', fopen('php://stderr', 'w'));
-        require './vendor/autoload.php';
-        use Workerman\Worker;
-        
-        \$server = new Worker('$serverAddress');
-        \$server->onMessage = function (\$connection, \$data) {
-            \$connection->send('received: '.\$data);
-        };
-        Worker::\$command = 'start';
-        Worker::runAll();
-    PHP
-    );
+beforeAll(function () use (&$process) {
+    $process = new PhpProcess(file_get_contents(__DIR__ . '/Stub/UdpServer.php'));
     $process->start();
-    sleep(1);
+    usleep(250000);
 });
 
 afterAll(function () use (&$process) {
@@ -31,8 +14,8 @@ afterAll(function () use (&$process) {
     $process->stop();
 });
 
-it('tests udp connection', function () use ($serverAddress) {
-    $socket = stream_socket_client($serverAddress, $errno, $errstr, 1);
+it('tests udp connection', function () {
+    $socket = stream_socket_client('udp://127.0.0.1:8083', $errno, $errstr, 1);
     expect($errno)->toBeInt()->toBe(0);
     fwrite($socket, 'xiami');
     $data = fread($socket, 1024);
