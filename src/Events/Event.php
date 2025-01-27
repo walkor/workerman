6dc10b36269f44e9eb8ea80c69d5862e3e2eecca
walkor <walkor@workerman.net>
2025-01-27 23:39:16 +0800

    Fix Timer::repeat for event

--- src/Events/Event.php
+++ src/Events/Event.php
@@ -140,7 +140,9 @@ final class Event implements EventInterface
     {
         $className = $this->eventClassName;
         $timerId = $this->timerId++;
-        $event = new $className($this->eventBase, -1, $className::TIMEOUT | $className::PERSIST, $func);
+        $event = new $className($this->eventBase, -1, $className::TIMEOUT | $className::PERSIST, function () use ($func, $args) {
+            $this->safeCall($func, $args);
+        });
         if (!$event->addTimer($interval)) {
             throw new \RuntimeException("Event::addTimer($interval) failed");
         }
