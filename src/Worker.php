60f59224c0027d5e9793204206b97582271208e8
落月 <102944161+lvluoyue@users.noreply.github.com>
2025-11-22 20:31:59 +0800

    fix: Cannot represent a stream of type phar stream as a select()able descriptor

--- src/Worker.php
+++ src/Worker.php
@@ -1607,7 +1607,7 @@ class Worker
             // Compatibility with the bug in Swow where the first request on Windows fails to trigger stream_select.
             if (extension_loaded('swow')) {
                 Timer::delay(0.1 , function(){
-                    $stream = fopen(__FILE__, 'r');
+                    $stream = fopen('php://memory', 'r');
                     static::$globalEvent->onReadable($stream, function($stream) {
                         static::$globalEvent->offReadable($stream);
                     });
