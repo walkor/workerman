340b55d32c53986a3481124c556fcfba912b8f2f
Joan Miquel <joanhey@kumbiaphp.com>
2026-03-18 11:41:22 +0100

    Revert last commit

--- src/Protocols/Ws.php
+++ src/Protocols/Ws.php
@@ -301,8 +301,9 @@ class Ws
             default => substr($bytes, 2),
         };
 
-        if ($connection->context->websocketCurrentFrameLength) {           
-            return $connection->context->websocketDataBuffer .= $decodedData;
+        if ($connection->context->websocketCurrentFrameLength) {
+            $connection->context->websocketDataBuffer .= $decodedData;
+            return $connection->context->websocketDataBuffer;
         }
 
         if ($connection->context->websocketDataBuffer !== '') {
